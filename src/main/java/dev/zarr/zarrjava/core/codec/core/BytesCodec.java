9dc2b407ac8cb79a912585e60fa84ded7ba3b3bd
brokkoli71 <brokkoli71b@gmail.com>
2025-10-13 15:18:40 +0200

    fix BytesCodec for boolean

--- src/main/java/dev/zarr/zarrjava/core/codec/core/BytesCodec.java
+++ src/main/java/dev/zarr/zarrjava/core/codec/core/BytesCodec.java
@@ -3,6 +3,8 @@ package dev.zarr.zarrjava.core.codec.core;
 import com.fasterxml.jackson.annotation.JsonValue;
 import dev.zarr.zarrjava.core.codec.ArrayBytesCodec;
 import ucar.ma2.Array;
+import ucar.ma2.DataType;
+import ucar.ma2.Index;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -11,12 +13,25 @@ public abstract class BytesCodec extends ArrayBytesCodec {
     protected abstract ByteOrder getByteOrder();
 
     @Override
-    public Array decode(ByteBuffer chunkBytes) {
-        chunkBytes.order(getByteOrder());
-        return Array.factory(arrayMetadata.dataType.getMA2DataType(), arrayMetadata.chunkShape,
-            chunkBytes);
+public Array decode(ByteBuffer chunkBytes) {
+    chunkBytes.order(getByteOrder());
+    DataType dtype = arrayMetadata.dataType.getMA2DataType();
+    int[] shape = arrayMetadata.chunkShape;
+
+    // Array.factory does not support boolean arrays directly from ByteBuffer
+    if (dtype == DataType.BOOLEAN) {
+        int size = chunkBytes.remaining();
+        boolean[] bools = new boolean[size];
+        for (int i = 0; i < size; i++) {
+            bools[i] = chunkBytes.get(i) != 0;
+        }
+
+        Index index = Index.factory(shape);
+        return Array.factory(DataType.BOOLEAN, index, bools);
     }
 
+    return Array.factory(dtype, shape, chunkBytes);
+}
     @Override
     public ByteBuffer encode(Array chunkArray) {
         return chunkArray.getDataAsByteBuffer(getByteOrder());
