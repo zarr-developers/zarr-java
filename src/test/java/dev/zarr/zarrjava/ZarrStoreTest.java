6689585da9c5bf87d3d5d4e486ef1f0942020f6b
Norman Rzepka <code@normanrz.com>
2025-12-19 13:16:21 +0100

    use different S3 source for testing

--- src/test/java/dev/zarr/zarrjava/ZarrStoreTest.java
+++ src/test/java/dev/zarr/zarrjava/ZarrStoreTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.zarr.zarrjava.core.Attributes;
 import dev.zarr.zarrjava.store.*;
 import dev.zarr.zarrjava.v3.*;
+import java.net.URI;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,6 +12,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -61,25 +63,31 @@ public class ZarrStoreTest extends ZarrTest {
     @Test
     public void testS3Store() throws IOException, ZarrException {
         S3Store s3Store = new S3Store(S3Client.builder()
-            .region(Region.of("eu-west-1"))
+            .endpointOverride(URI.create("https://uk1s3.embassy.ebi.ac.uk"))
+            .region(Region.US_EAST_1) // required, but ignored
+            .serviceConfiguration(
+                S3Configuration.builder()
+                    .pathStyleAccessEnabled(true) // required
+                    .build()
+            )
             .credentialsProvider(AnonymousCredentialsProvider.create())
-            .build(), "static.webknossos.org", "data");
+            .build(), "idr", "zarr/v0.5/idr0033A");
 
-        Array arrayV3 = Array.open(s3Store.resolve("zarr_v3", "l4_sample", "color", "1"));
-        Assertions.assertArrayEquals(new long[]{1, 4096, 4096, 2048}, arrayV3.metadata().shape);
-        Assertions.assertEquals(0, arrayV3.read(new long[]{0,0,0,0}, new int[]{1,1,1,1}).getInt(0));
+        Array arrayV3 = Array.open(s3Store.resolve("BR00109990_C2.zarr", "0", "0"));
+        Assertions.assertArrayEquals(new long[]{5, 1552, 2080}, arrayV3.metadata().shape);
+        Assertions.assertEquals(574, arrayV3.read(new long[]{0,0,0}, new int[]{1,1,1}).getInt(0));
 
-        dev.zarr.zarrjava.core.Array arrayCore = dev.zarr.zarrjava.core.Array.open(s3Store.resolve("zarr_v3", "l4_sample", "color", "1"));
-        Assertions.assertArrayEquals(new long[]{1, 4096, 4096, 2048}, arrayCore.metadata().shape);
-        Assertions.assertEquals(0, arrayCore.read(new long[]{0,0,0,0}, new int[]{1,1,1,1}).getInt(0));
+        dev.zarr.zarrjava.core.Array arrayCore = dev.zarr.zarrjava.core.Array.open(s3Store.resolve("BR00109990_C2.zarr", "0", "0"));
+        Assertions.assertArrayEquals(new long[]{5, 1552, 2080}, arrayCore.metadata().shape);
+        Assertions.assertEquals(574, arrayCore.read(new long[]{0,0,0}, new int[]{1,1,1}).getInt(0));
     }
 
     @Test
     public void testHttpStore() throws IOException, ZarrException {
-        HttpStore httpStore = new dev.zarr.zarrjava.store.HttpStore("https://static.webknossos.org/data/zarr_v3/l4_sample");
-        Array array = Array.open(httpStore.resolve("color", "1"));
+        HttpStore httpStore = new dev.zarr.zarrjava.store.HttpStore("https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.5/idr0033A");
+        Array array = Array.open(httpStore.resolve("BR00109990_C2.zarr", "0", "0"));
 
-        Assertions.assertArrayEquals(new long[]{1, 4096, 4096, 2048}, array.metadata().shape);
+        Assertions.assertArrayEquals(new long[]{5, 1552, 2080}, array.metadata().shape);
     }
 
     @ParameterizedTest
