abc93b428106de04aed90049df340cde58c9d7b8
Konstantin <konsti.bobenko@gmail.com>
2026-09-08 15:01:42 +0200

    made gzip work for v2

--- src/main/java/dev/zarr/zarrjava/v3/codec/core/GzipCodec.java
+++ src/main/java/dev/zarr/zarrjava/v3/codec/core/GzipCodec.java
@@ -4,20 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.zarr.zarrjava.ZarrException;
-import dev.zarr.zarrjava.core.codec.BytesBytesCodec;
-import dev.zarr.zarrjava.utils.Utils;
 import dev.zarr.zarrjava.v3.ArrayMetadata;
 import dev.zarr.zarrjava.v3.codec.Codec;
 
 import javax.annotation.Nonnull;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
-public class GzipCodec extends BytesBytesCodec implements Codec {
+public class GzipCodec extends dev.zarr.zarrjava.core.codec.core.GzipCodec implements Codec {
 
     @JsonIgnore
     public final String name = "gzip";
@@ -30,31 +23,9 @@ public class GzipCodec extends BytesBytesCodec implements Codec {
         this.configuration = configuration;
     }
 
-
     @Override
-    public ByteBuffer decode(ByteBuffer chunkBytes)
-            throws ZarrException {
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); GZIPInputStream inputStream = new GZIPInputStream(
-                new ByteArrayInputStream(Utils.toArray(chunkBytes)))) {
-            Utils.copyStream(inputStream, outputStream);
-            inputStream.close();
-            return ByteBuffer.wrap(outputStream.toByteArray());
-        } catch (IOException ex) {
-            throw new ZarrException("Error in decoding gzip.", ex);
-        }
-    }
-
-    @Override
-    public ByteBuffer encode(ByteBuffer chunkBytes)
-            throws ZarrException {
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); GZIPOutputStream gzipStream = new GZIPOutputStream(
-                outputStream)) {
-            gzipStream.write(Utils.toArray(chunkBytes));
-            gzipStream.close();
-            return ByteBuffer.wrap(outputStream.toByteArray());
-        } catch (IOException ex) {
-            throw new ZarrException("Error in encoding gzip.", ex);
-        }
+    public ByteBuffer encode(ByteBuffer chunkBytes) throws ZarrException {
+        return encodeInternal(configuration.level, chunkBytes);
     }
 
     @Override
@@ -77,5 +48,3 @@ public class GzipCodec extends BytesBytesCodec implements Codec {
         }
     }
 }
-
-
